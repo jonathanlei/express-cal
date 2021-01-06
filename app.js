97d0cb70f773af587d63c47fe7e842879ca09edc
@jonathanlei <jonathanlei98@gmail.com>
2021-01-06 15:25:33 -0500

    finished all

--- app.js
+++ app.js
@@ -4,20 +4,97 @@ const express = require("express");
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
+
+const {convertStrNums} = require("./utils.js");
+const {
+  findMean,
+  findMedian,
+  findMode,
+} = require("./stats.js");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
+app.use(express.json());
+
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-
+app.get("/mean", function(req, res, next){
+  let numsStr = req.query.nums;
+  if (!numsStr){
+    return next(new BadRequestError("nums are required"));
+  }
+  let strNums = numsStr.split(",");
+  let numList;
+  try{
+    numList = convertStrNums(strNums);
+  }catch(err){
+    return next(err);
+  }
+  let mean = findMean(numList);
+  return res.json({operation: "mean", value: mean})
+})
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
 
+app.get("/median", function(req, res, next){
+  let numsStr = req.query.nums;
+  if (!numsStr){
+    return next(new BadRequestError("nums are required"));
+  }
+  let strNums = numsStr.split(",");
+  let numList;
+  try{
+    numList = convertStrNums(strNums);
+  }catch(err){
+    return next(err);
+  }
+  let median = findMedian(numList);
+  return res.json({operation: "median", value: median})
+})
 
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
+
+/** Finds mode of nums in qs: returns {operation: "mode", result } */
+
+app.get("/mode", function(req, res, next){
+  let numsStr = req.query.nums;
+  if (!numsStr){
+    return next(new BadRequestError("nums are required"));
+  }
+  let strNums = numsStr.split(",");
+  let numList;
+  try{
+    numList = convertStrNums(strNums);
+  }catch(err){
+    return next(err);
+  }
+  let mode = findMode(numList);
+  return res.json({operation: "all", value: mode})
+})
 
 
+/*  Finds mean, median and mode of nums in qs: returns {operation: "all", results } */
+
+app.get("/all", function(req, res, next){
+  let numsStr = req.query.nums;
+  if (!numsStr){
+    return next(new BadRequestError("nums are required"));
+  }
+  let strNums = numsStr.split(",");
+  let numList;
+  try{
+    numList = convertStrNums(strNums);
+  }catch(err){
+    return next(err);
+  }
+  let mean = findMean(numList);
+  let median = findMedian(numList);
+  let mode = findMode(numList);
+  return res.json({operation: "all",
+                   mean,
+                   median,
+                   mode})
+})
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
   return next(new NotFoundError());
@@ -30,6 +107,7 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
+
 
 
 
--- utils.js
+++ utils.js
@@ -6,6 +6,15 @@ const { BadRequestError } = require("./expressError");
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  let numsList = [];
+  for (let n of strNums){
+    if (isNaN(parseInt(n))){
+      throw new BadRequestError( `${n} is not a number`);
+    }else{
+      numsList.push(Number(n));
+    }
+  }
+  return numsList;
 }
 
 
